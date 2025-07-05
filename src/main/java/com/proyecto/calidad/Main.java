639ea41f40fa88aa794574dd9b418ec3eb68874b
MateoCartagena <mateo.cartagena@udla.edu.ec>
2025-07-05 16:23:44 -0500

    cambios cd

--- src/main/java/com/proyecto/calidad/Main.java
+++ src/main/java/com/proyecto/calidad/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) {
         try (PrintWriter w = new PrintWriter("output/index.html", "UTF-8")) {
             w.println("<h1>Hello from Java CI/CD!</h1>");
-            w.println("<p><a href=\"../target/site/checkstyle.html\">Ver reporte Checkstyle</a></p>");
-            w.println("<p><a href=\"../target/site/pmd.html\">Ver reporte PMD</a></p>");
-            w.println("<p><a href=\"../target/site/jacoco/index.html\">Ver reporte JaCoCo</a></p>");
+            w.println("<p><a href=\"checkstyle.html\">Ver reporte Checkstyle</a></p>");
+            w.println("<p><a href=\"pmd.html\">Ver reporte PMD</a></p>");
+            w.println("<p><a href=\"jacoco/index.html\">Ver reporte JaCoCo</a></p>");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
